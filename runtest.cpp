e9533b745dc064b5df19b8bf44c049b90954411d
Adam Chlebek <adam.chlebek@dominionsystems.local>
2019-11-05 14:55:46 -0500

    Updated RunTestCPP and config

--- runtest.cpp
+++ runtest.cpp
@@ -539,9 +539,9 @@ TEST_CASE("Operator +") {
         Fraction one{"2 6/13"};
         Fraction sum;
 
-        sum = -14 + one; // -12 6/13
-        CHECK(sum.whole() == 12);
-        CHECK(sum.numerator() == 6);
+        sum = -14 + one; // -11 7/13
+        CHECK(sum.whole() == 11);
+        CHECK(sum.numerator() == 7);
         CHECK(sum.denominator() == 13);
         CHECK_FALSE(sum.isPositive());
     }
@@ -597,8 +597,8 @@ TEST_CASE("Operator -") {
         Fraction sum;
 
         sum = one - 5;
-        CHECK(sum.whole() == 3);
-        CHECK(sum.numerator() == 6);
+        CHECK(sum.whole() == 2);
+        CHECK(sum.numerator() == 7);
         CHECK(sum.denominator() == 13);
         CHECK_FALSE(sum.isPositive());
     }
@@ -1063,7 +1063,5 @@ TEST_CASE("Operator()") {
         for (int k = 1; k < 20; k++)
             CHECK(zero(k) == "0");
     }
-
-
 }
 #endif
